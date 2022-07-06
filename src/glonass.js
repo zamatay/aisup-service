9d906353cecf6f07736c04110a560804e63152d1
vkbDevelop <vkbDevelop@v-k-b.ru>
2022-07-06 16:11:18 +0300

    add glonas sync

--- src/glonass.js
+++ src/glonass.js
@@ -1,10 +1,11 @@
 // подключаем необходимые модули
-const fileName = (process.env.NODE_ENV) ? `../.${process.env.NODE_ENV}.env` : '../.env';
-const fs = require('fs').promises;
 const path = require('path');
-const a = require('dotenv').config({path: fileName});
 const axios = require('axios/lib/axios');
 const typeorm = require("typeorm");
+
+//Подключаем файл конфигурации
+const fileName = (process.env.NODE_ENV) ? `../.${process.env.NODE_ENV}.env` : '../.env';
+require('dotenv').config({path: fileName});
 
 // создаем экземпляр для работы с запросами к сервису
 const http = axios.create({
@@ -36,7 +37,7 @@ const getPromise = (id)=>{
   return http.get(`http://vkb.glonass.io/api/integration/v1/objectinfo?oid=${id}&dt=${nowDate}`);
 }
 
-const query = async (query, parameters/*: object = {}*/)=>{
+const query = async (query, parameters)=>{
   const connection = dataSource.manager.connection;
   const [ escapedQuery, escapedParams ] = connection.driver.escapeQueryWithParameters(query, parameters, {});
   return connection.query(escapedQuery, escapedParams);
@@ -51,40 +52,44 @@ const getData = async (promises)=>{
   }
 }
 
+const sendMessage = async (message)=>{
+  const url = `https://api.telegram.org/${process.env.TELEGRAM_KEY}/sendmessage?chat_id=${process.env.TELEGRAM_CHAT_ID}&text=${encodeURI(message)}`;
+  http.get(url);
+}
+
 const start = async () => {
-  // логинимся
-  const data = await http.get('http://vkb.glonass.io/api/integration/v1/connect?login=%D0%92%D0%9A%D0%91&password=%D0%A2%D1%83%D0%BB%D0%B03&lang=ru-ru&timezone=0');
-  const sessionID = data.headers.sessionid;
-  // если залогинились и получили сессию
-  if (sessionID){
-    await dataSource.initialize();
-    http.defaults.headers.get = {COOKIE: `SGUID=session_id=${sessionID}`};
-    // получаем список объектов
-    const list = await http.get('http://vkb.glonass.io/api/integration/v1/getobjectslist?companyId=0');
-    await query('exec m_ImportGlonasTransport :data', {data: JSON.stringify(list.data)});
-    const ids = list.data.objects.map((item)=>item.id);
-    const allData = [];
-    // пока список не пуст запрпашиваем данные по объекту
-    while (ids.length){
-      const rawIDs = ids.splice(0, _CHUNK_DATA);
-      // формируем промисы
-      const promises = rawIDs.map(id=>getPromise(id));
-      let exceptCount = 0;
-      let objectData;
-      // ждем пока все выполнится без ошибок но не более 5 раз
-      while (!(objectData = await getData(promises)) || exceptCount < 5){
-        exceptCount++
+  try { // логинимся
+    const data = await http.get("http://vkb.glonass.io/api/integration/v1/connect?login=%D0%92%D0%9A%D0%91&password=%D0%A2%D1%83%D0%BB%D0%B03&lang=ru-ru&timezone=0");
+    const sessionID = data.headers.sessionid;
+    // если залогинились и получили сессию
+    if (sessionID) {
+      await dataSource.initialize();
+      http.defaults.headers.get = { COOKIE: `SGUID=session_id=${sessionID}` };
+      // получаем список объектов
+      const list = await http.get("http://vkb.glonass.io/api/integration/v1/getobjectslist?companyId=0");
+      await query("exec m_ImportGlonasTransport :data", { data: JSON.stringify(list.data) });
+      const ids = list.data.objects.map((item) => item.id);
+      const allData = [];
+      // пока список не пуст запрпашиваем данные по объекту
+      while (ids.length) {
+        const rawIDs = ids.splice(0, _CHUNK_DATA);
+        // формируем промисы
+        const promises = rawIDs.map(id => getPromise(id));
+        let exceptCount = 0;
+        let objectData;
+        // ждем пока все выполнится без ошибок но не более 5 раз
+        while (!(objectData = await getData(promises)) || exceptCount < 5) {
+          exceptCount++;
+        }
+        // собираем в объект
+        objectData.forEach(item => allData.push(item.data));
       }
-      // собираем в объект
-      objectData.forEach(item=>allData.push(item.data));
+      const file = path.join(__dirname, "objectData.log");
+      await query("exec m_ImportGlonasTransportDetail :data", { data: JSON.stringify(allData) });
+      sendMessage("Выгрузка Глонас завершена");
     }
-    const file = path.join(__dirname, 'objectData.log');
-    await query('exec m_ImportGlonasTransportDetail :data', {data: JSON.stringify(allData)});
-    // await fs.writeFile(file, JSON.stringify(allData), (er)=>{
-    //   if (er) {
-    //     console.log(er);
-    //   }
-    // })
+  } catch (e) {
+    sendMessage(`Произошла ошибка во время выполнения импорта данных из Глонасс\r\n${e.message}`)
   }
 }
 
