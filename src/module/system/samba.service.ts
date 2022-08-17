ff9e7c632eb4cd4dc710a4d81fcf51a720bd23b2
vkbDevelop <vkbDevelop@v-k-b.ru>
2022-08-17 13:26:02 +0300

    repair bug roistat

--- src/module/system/samba.service.ts
+++ src/module/system/samba.service.ts
@@ -8,29 +8,42 @@ export class SambaService extends BaseService{
 
     constructor(props) {
         super(props);
-        const SMB2 = require('smb2');
-        this.smb = new SMB2({
-            share: "\\\\192.168.0.8\\Images"
-            , domain:"vkbdisk"
-            , username:"ApacheImages333"
-            , password:"HJDfgt67YudF78@"
-            , autoCloseTimeout: 0
-        })
+    }
+
+    getSamba(fileName){
+        const SMB2 = require('@marsaud/smb2');
+
+        const allowShare = [
+            '\\\\192.168.0.8\\Images', '\\\\192.168.0.8\\Документы', '\\\\192.168.0.8\\Общедоступные документы',
+            '\\\\vkbdisk\\Images', '\\\\vkbdisk\\Документы', '\\\\vkbdisk\\Общедоступные документы'
+        ];
+        const idx = allowShare.findIndex(item=>fileName.toLowerCase().startsWith(item.toLowerCase()));
+
+        if (idx >= 0) {
+            const share = allowShare[idx].replace('vkbdisk', '192.168.0.8')
+            fileName = fileName.substring(allowShare[idx].length + 1).replace(/\\/g, '/');
+            return [new SMB2({
+                share
+                , domain: "vkbdisk"
+                //, username: "admin"
+                //, password: "ldytbcyc"
+                , username: 'ApacheImages333'
+                , password: 'HJDfgt67YudF78@'
+            }), fileName];
+        }
+        return null;
     }
 
     async readFile(fileName: string): Promise<any>{
-        const startWith = '\\\\vkbdisk\\images\\';
-        if (fileName.startsWith(startWith)){
-            fileName = fileName.substring(startWith.length);
-        }
-        fileName = fileName.replace(/\\/g, '/');
-
+        const [smb, file] = this.getSamba(fileName);
         return await new Promise((resolve, reject)=>{
-            this.smb.readFile(fileName, (err, data: Buffer)=>{
-                if (err) reject(err);
-                console.log(data.constructor);
-                resolve(data);
-            })
+            if (smb){
+                smb.readFile(file, (err, data: Buffer)=>{
+                    if (err) reject(err);
+                    resolve(data);
+                })
+            } else
+                resolve(false);
         })
     }
 }
--- src/module/system/system.controller.ts
+++ src/module/system/system.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Query, StreamableFile } from "@nestjs/common";
+import { Controller, Get, Query, Res, StreamableFile } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { createReadStream } from "fs";
 import { BaseService } from "../../services/base.service";
 import { SambaService } from "./samba.service";
 import { FileService } from "./file.service";
+import { Response } from "express";
 
 @ApiTags('Системные функции')
 @Controller('api/system')
@@ -16,9 +17,14 @@ export class SystemController extends BaseService{
     @ApiOperation({ summary: 'Получение файла по id в базе' })
     @ApiResponse({ status: 200, description: 'Возвращает содержимое файла' })
     @Get('fileById')
-    async getFileById(@Query('id') id: number): Promise<StreamableFile> {
+    async getFileById(@Query('id') id: number, @Res({ passthrough: true }) res: Response): Promise<StreamableFile> {
         const fileName = await this.fileService.getFileNameByID(id);
+        const file = encodeURIComponent(fileName.split('\\').slice(-1)[0]);
         const readStream = await this.sambaService.readFile(fileName);//createReadStream(await this.sambaService.readFile(fileName));
-        return new StreamableFile(readStream);
+        res.set({
+            'Content-Disposition': `attachment; filename="${file}"`,
+        });
+        //res.setHeader("Content-disposition",`attachment; filename=${file}`);
+        return new StreamableFile(readStream/*, {disposition: `filename=${file}`}*/);
     }
 }
