e34d71a482a8972bf38b0ceca64f449528e0668f
vkbDevelop <vkbDevelop@v-k-b.ru>
2022-08-18 16:26:59 +0300

    add roistat send status

--- src/module/system/file.service.ts
+++ src/module/system/file.service.ts
@@ -3,11 +3,12 @@ import { BaseService } from "../../services/base.service";
 
 @Injectable()
 export class FileService extends BaseService{
-    async getFileNameByID(id: number): Promise<string | null>{
-        const data = await  this.manager.createQueryBuilder()
+    async getFileNameByID(id: number[]): Promise<string[]>{
+        const query = this.manager.createQueryBuilder()
             .from('_Files', 'f')
-            .where('id=:id', { id })
-            .getRawOne();
-        return data.FileName;
+            .where('id in (:...id)', { id });
+        const data = await query.execute();
+        console.log(data);
+        return data.map(i=>i.FileName);
     }
 }
--- src/module/system/system.controller.ts
+++ src/module/system/system.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query, Res, StreamableFile } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Query, Res, StreamableFile } from "@nestjs/common";
 import { ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { createReadStream } from "fs";
 import { BaseService } from "../../services/base.service";
@@ -17,14 +17,32 @@ export class SystemController extends BaseService{
     @ApiOperation({ summary: 'Получение файла по id в базе' })
     @ApiResponse({ status: 200, description: 'Возвращает содержимое файла' })
     @Get('fileById')
-    async getFileById(@Query('id') id: number, @Res({ passthrough: true }) res: Response): Promise<StreamableFile> {
-        const fileName = await this.fileService.getFileNameByID(id);
-        const file = encodeURIComponent(fileName.split('\\').slice(-1)[0]);
-        const readStream = await this.sambaService.readFile(fileName);//createReadStream(await this.sambaService.readFile(fileName));
-        res.set({
-            'Content-Disposition': `attachment; filename="${file}"`,
-        });
-        //res.setHeader("Content-disposition",`attachment; filename=${file}`);
-        return new StreamableFile(readStream/*, {disposition: `filename=${file}`}*/);
+    async fileById(@Query('id') id: number, @Res({ passthrough: true }) res: Response): Promise<StreamableFile> {
+        const fileName = await this.fileService.getFileNameByID([id]);
+        if (fileName && fileName.length){
+            try {
+                const file = encodeURIComponent(fileName[0].split('\\').slice(-1)[0]);
+                const readStream = await this.sambaService.readFile(fileName[0]);//createReadStream(await this.sambaService.readFile(fileName));
+                res.set({
+                    'Content-Disposition': `attachment; filename="${file}"`,
+                });
+                return new StreamableFile(readStream);
+            } catch (e){
+                throw new NotFoundException({ message: "Файл не найден" })
+            }
+        } else
+            throw new NotFoundException({ message: "Файл не найден" })
+    }
+
+    @ApiOperation({ summary: 'Получение имени файла' })
+    @ApiResponse({ status: 200, description: 'Возвращает имя по его id' })
+    @Get('fileNameById')
+    async fileNameById(@Query('id') id: number[]): Promise<any> {
+        try {
+            const filesFull = await this.fileService.getFileNameByID(id);
+            return filesFull.map(item=>item.split('\\').slice(-1)[0])
+        }catch (e) {
+            throw new NotFoundException({ message: "Файл не найден" })
+        }
     }
 }
